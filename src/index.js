0b479ce8c1840efd286b98d372d4dc9749dac5b4
hectorcoronado <coronado.hector@gmail.com>
2017-05-08 00:23:10 -0700

    creates dummy Welcome component & renders it on IndexRoute

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reduxThunk from 'redux-thunk'
 
 import App from './components/App';
 import Feature from './components/Feature';
+import Welcome from './components/Welcome';
 import RequireAuth from './components/auth/RequireAuth';
 import Signin from './components/auth/Signin';
 import Signout from './components/auth/Signout';
@@ -19,6 +20,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
+        <IndexRoute component={Welcome} />
         <Route path="signin" component={Signin} />
         <Route path="signout" component={Signout} />
         <Route path="signup" component={Signup} />
